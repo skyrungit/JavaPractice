2fdbd45f1ebcc73a742ba1adde2d3ffe0d20a82d
SKY <3210654789@naver.com>
2022-10-19 21:59:31 +0900

    Max date of a month

--- calender/src/sky/calender/Calender.java
+++ calender/src/sky/calender/Calender.java
@@ -1,12 +1,25 @@
 package sky.calender;
 
+import java.util.Scanner;
+
 public class Calender {
     public static void main(String[] args) {
+        
         System.out.println("일 월 화 수 목 금 토");
         System.out.println("--------------------");
         System.out.println("1  2  3  4  5  6  7");
         System.out.println("8  9 10 11 12 13 14");
         System.out.println("15 16 17 18 19 20 21");
         System.out.println("22 23 24 25 26 27 28");
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input Month : ");
+        int month = sc.nextInt();
+        
+        int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        System.out.printf("%d Month has max date %d. \n", month, maxDays[month-1]);
+        
+        sc.close();
     }
 }
