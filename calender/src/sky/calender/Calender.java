269f8dd353a1093f0fdb5d0e026dd5ab785ddcf2
SKY <3210654789@naver.com>
2022-10-22 18:59:32 +0900

    Calender "For"

--- calender/src/sky/calender/Calender.java
+++ calender/src/sky/calender/Calender.java
@@ -10,7 +10,7 @@ public class Calender {
     }
 
     public static void main(String[] args) {
-   
+    
         System.out.println("일 월 화 수 목 금 토");
         System.out.println("--------------------");
         System.out.println("1  2  3  4  5  6  7");
