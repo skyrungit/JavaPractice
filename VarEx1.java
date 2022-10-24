78ac87fd8436ef55b02084da4861b22a6e379228
SKY <3210654789@naver.com>
2022-10-25 00:53:41 +0900

    14 -> 10

--- VarEx1.java
+++ VarEx1.java
@@ -2,7 +2,7 @@
 public class VarEx1 {
 	public static void main(String[] args) {
         int year = 0;
-        int age = 14;
+        int age = 10;
 
         System.out.println(year);
         System.out.println(age);
